265e337022434326cc68d9cb7ce3a229506fa475
blazer1400 <sbroersma@roc-dev.com>
2020-09-13 22:19:50 +0200

    inleiding java

---
+++ src/h08/BTW.java
@@ -0,0 +1,46 @@
+package h08;
+import java.awt.*;
+import java.applet.*;
+import java.awt.event.*;
+
+public class BTW extends Applet {
+
+    String stringgeld;
+    TextField input;
+    Button confirm;
+    double bedrag;
+    String display;
+    Label label;
+
+
+    public void init() {
+        input = new TextField("",20);
+
+        confirm = new Button("Ok");
+        confirm.addActionListener(new Calc());
+
+        label = new Label("Typ een bedrag:");
+
+        display = "";
+
+        add(label);
+        add(input);
+        add(confirm);
+    }
+
+    public void paint(Graphics g) {
+        g.drawString(display,50,40);
+    }
+
+    class Calc implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            stringgeld = input.getText();
+            bedrag = Double.parseDouble(stringgeld);
+            double btwper = bedrag / 100 * 21;
+            bedrag = bedrag + btwper;
+            display = "" + bedrag;
+            repaint();
+        }
+    }
+
+}
---
+++ src/h08/Tekstvak.java
@@ -0,0 +1,56 @@
+package h08;
+import java.awt.*;
+import java.applet.*;
+import java.awt.event.*;
+
+public class Tekstvak extends Applet {
+
+    Button ok;
+    Button reset;
+    String displayTekst;
+    TextField tekstvak;
+
+    public void init() {
+
+        tekstvak = new TextField("",20);
+
+        KnopListener kl = new KnopListener();
+        ok = new Button();
+        ok.addActionListener(kl);
+        ok.setLabel("Ok");
+
+        ResetEvent bl = new ResetEvent();
+        reset = new Button();
+        reset.setLabel("Reset");
+        reset.addActionListener(bl);
+
+        displayTekst = "";
+
+        add(tekstvak);
+        add(ok);
+        add(reset);
+
+    }
+
+    public void paint(Graphics g) {
+
+        g.drawString("" + displayTekst, 10, 40);
+
+
+    }
+
+    class KnopListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            String output = tekstvak.getText();
+            displayTekst = "" + output;
+            repaint();
+        }
+    }
+    class ResetEvent implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            displayTekst = "";
+            repaint();
+        }
+    }
+
+}
