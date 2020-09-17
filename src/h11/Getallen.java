5ff9c025705326b144973cd5fda90d23293f722b
blazer1400 <sbroersma@roc-dev.com>
2020-09-17 10:22:24 +0200

    opdracht 11.1 en 11.2 af

---
+++ src/h11/Getallen.java
@@ -0,0 +1,18 @@
+package h11;
+import java.applet.*;
+import java.awt.*;
+
+public class Getallen extends Applet {
+
+    public void paint(Graphics g) {
+
+        int teller;
+        int y = 20;
+
+        for(teller = 20; teller >= 10; teller--) {
+            g.drawString("" + teller, 30, y);
+            y += 20;
+        }
+    }
+
+}
--- src/h11/Lijnen.java
+++ src/h11/Lijnen.java
@@ -1,20 +1,16 @@
 package h11;
 import java.applet.*;
 import java.awt.*;
-import java.awt.event.*;
 
 public class Lijnen extends Applet {
 
-    int teller = 0;
-    int y = 20;
-
-    public void init() {
-    }
-
     public void paint(Graphics g) {
+        int teller;
+        int y = 0;
+
         for (teller = 0; teller <= 10; teller++) {
-            g.drawLine(50, y, 150, y);
             y += 20;
+            g.drawLine(50, y, 150, y);
         }
     }
 
