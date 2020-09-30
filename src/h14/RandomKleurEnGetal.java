9326763f591ac52d1ca53c86cf97b76e64b893be
blazer1400 <sbroersma@roc-dev.com>
2020-09-30 17:37:13 +0200

    14.1

--- src/h14/RandomKleurEnGetal.java
+++ src/h14/RandomKleurEnGetal.java
@@ -7,7 +7,8 @@ public class RandomKleurEnGetal extends Applet {
 
     int kleurgetal;
     int getal = 0;
-    Color kleur = Color.black;
+    int kleurarray;
+    Color kleur[] = {Color.red, Color.blue, Color.green, Color.yellow, Color.magenta, Color.cyan};
 
     public void init() {
         Button confirm = new Button("spin");
@@ -16,7 +17,7 @@ public class RandomKleurEnGetal extends Applet {
     }
 
     public void paint(Graphics g) {
-        g.setColor(kleur);
+        g.setColor(kleur[kleurarray - 1]);
         g.fillRect(50,50,20,20);
         g.setColor(Color.black);
         g.setFont(new Font("font", Font.PLAIN, 20));
@@ -31,22 +32,22 @@ public class RandomKleurEnGetal extends Applet {
             getal = (int) (random2 * 9 + 1);
             switch(kleurgetal) {
                 case 1:
-                    kleur = Color.red;
+                    kleurarray = 1;
                     break;
                 case 2:
-                    kleur = Color.blue;
+                    kleurarray = 2;
                     break;
                 case 3:
-                    kleur = Color.green;
+                    kleurarray = 3;
                     break;
                 case 4:
-                    kleur = Color.yellow;
+                    kleurarray = 4;
                     break;
                 case 5:
-                    kleur = Color.magenta;
+                    kleurarray = 5;
                     break;
                 case 6:
-                    kleur = Color.cyan;
+                    kleurarray = 6;
                     break;
             }
             repaint();
